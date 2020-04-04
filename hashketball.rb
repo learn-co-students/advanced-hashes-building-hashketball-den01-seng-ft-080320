806b8ad0d2bb8516c4e90115553241e7cd1015a7
Austen Bright <pureaustenbright@gmail.com>
2020-04-04 06:05:36 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -70,6 +70,7 @@ def game_hash
       :blocks => 11,
       :slam_dunks => 1
       }]
+    }
   :away => {
     :team_name => "Charlotte Hornets",
     :colors => "Turquoise, Purple",
@@ -129,4 +130,5 @@ def game_hash
       :slam_dunks => 12
       }]
   }
+}
 end
