c00386bf2e4fd772295a7c257154f56d48c1b655
Thomas Bachorz <tjbachorz@gmail.com>
2020-06-11 17:10:24 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,119 @@
 # Write your code here!
+def game_hash
+  return_hash = {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [
+        {
+          :player_name => "",
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        }, {
+          :player_name => "",
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        }, {
+          :player_name => "",
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        }, {
+          :player_name => "",
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        }, {
+          :player_name => "",
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        }
+      ]
+    },
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => [
+        {
+          :player_name => "Jeff Adrien",
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2
+        }, {
+          :player_name => "",
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        }, {
+          :player_name => "",
+          :number => 0,
+          :shoe => 0,
+          :points => 0,
+          :rebounds => 0,
+          :assists => 0,
+          :steals => 0,
+          :blocks => 0,
+          :slam_dunks => 0
+        }, {
+          :player_name => "Ben Gordon",
+          :number => 8,
+          :shoe => 15,
+          :points => 33,
+          :rebounds => 3,
+          :assists => 2,
+          :steals => 1,
+          :blocks => 1,
+          :slam_dunks => 0
+        }, {
+          :player_name => "Kemba Walker",
+          :number => 33,
+          :shoe => 15,
+          :points => 6,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 7,
+          :blocks => 5,
+          :slam_dunks => 12
+        }
+      ]
+    }
+  }
+end 
